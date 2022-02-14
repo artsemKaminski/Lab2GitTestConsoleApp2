46f90ed0608be86fd4430e56b4ba4ed737e31bf8
Artsem Kaminski <artsem.kaminski@leverx-group.com>
2022-02-14 21:32:49 +0300

    Add properties to TestClass

--- Lab2GitTestConsoleApp2/TestClass.cs
+++ Lab2GitTestConsoleApp2/TestClass.cs
@@ -9,5 +9,8 @@ namespace Lab2GitTestConsoleApp2
     public class TestClass
     {
         public string TestProperty { get; set; }
+        public string TestProperty1 { get; set; }
+        public string TestProperty2 { get; set; }
+        public string TestProperty3 { get; set; }
     }
 }
